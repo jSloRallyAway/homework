2f94cd50c462a89b6313c6447dacdacbbab9c331
Justin Sloane <jslo@rallyaway.com>
2017-08-22 14:21:04 -0400

    Homework 3 finished.

---
+++ homework-3-app/app/helpers/homework_controller_helper.rb
@@ -0,0 +1,2 @@
+module HomeworkControllerHelper
+end
---
+++ homework-3-app/test/controllers/homework_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HomeworkControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
